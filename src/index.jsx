3747f993298eed8d5f57738380eae0af348f631a
Matt Yoder <mjyoder@psc.edu>
2024-09-05 17:06:10 -0400

    feat: add elapsed seconds prop and render HH:MM:SS

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,13 @@
 import { render } from "preact";
 import "./style.css";
 
-export function App() {
-  return "Hello world!";
+export function Duration({ elapsedSeconds }) {
+  const hours = Math.floor(elapsedSeconds / 3600);
+  const minutes = Math.floor((elapsedSeconds % 3600) / 60);
+  const seconds = elapsedSeconds % 60;
+  return [hours, minutes, seconds]
+    .map((number) => number.toString().padStart(2, "0"))
+    .join(":");
 }
 
-render(<App />, document.getElementById("app"));
+render(<Duration elapsedSeconds={330} />, document.getElementById("app"));
